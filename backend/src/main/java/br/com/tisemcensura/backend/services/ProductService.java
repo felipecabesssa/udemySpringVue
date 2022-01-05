8c6b69d5f8376cb4b4acf3391d3e0ad5e6c14550
felipecabesssa <felipesoares100@hotmail.com>
2022-01-05 01:03:42 -0300

    CRUD - Criando Product Service - copia e cola e replace category por product

---
+++ backend/src/main/java/br/com/tisemcensura/backend/services/ProductService.java
@@ -0,0 +1,39 @@
+package br.com.tisemcensura.backend.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.tisemcensura.backend.entities.Product;
+import br.com.tisemcensura.backend.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Optional<Product> findById(Long id){
+		Optional<Product> obj = repository.findById(id);
+		return obj;
+	}
+	
+	public Product create(Product Product) {
+		return repository.save(Product);
+	}
+	
+	public Product update(Product Product, Long id) {
+		Product.setId(id);
+		return repository.save(Product);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+}
