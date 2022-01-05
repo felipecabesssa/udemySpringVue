ef7db7f2b53b790698da0a4ce5777b7787740a27
felipecabesssa <felipesoares100@hotmail.com>
2022-01-04 23:46:44 -0300

    Inicio Projeto Backend

---
+++ backend/src/test/java/br/com/tisemcensura/backend/BackendFullstackCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tisemcensura.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFullstackCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
