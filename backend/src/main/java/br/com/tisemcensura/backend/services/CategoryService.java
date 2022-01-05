63e3f2b62efec8c6693ef322cd038184a83471a9
felipecabesssa <felipesoares100@hotmail.com>
2022-01-05 01:35:58 -0300

    CRUD - Criando Category Controller

--- backend/src/main/java/br/com/tisemcensura/backend/services/CategoryService.java
+++ backend/src/main/java/br/com/tisemcensura/backend/services/CategoryService.java
@@ -19,12 +19,12 @@ public class CategoryService {
 		return repository.findAll();
 	}
 	
-	public Optional<Category> findById(Long id){
+	public Category findById(Long id){
 		Optional<Category> obj = repository.findById(id);
-		return obj;
+		return obj.get();
 	}
 	
-	public Category create(Category category) {
+	public Category save(Category category) {
 		return repository.save(category);
 	}
 	
