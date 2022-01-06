a3ce3ced9fcbd5aba87b7410461d42dd3efa1b20
felipecabesssa <felipesoares100@hotmail.com>
2022-01-05 23:56:01 -0300

    Consumindo dados no Products de arquivo Json

--- frontend/src/views/Products.vue
+++ frontend/src/views/Products.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>
+  <div class="principal">
+    <div class="principal">
       <h1>Products</h1>
+      <div>
+        {{dados}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import dados from '../data/data.json'
 export default {
-
+  data(){
+    return{
+      dados:dados
+    }
+  }
 }
 </script>
 
