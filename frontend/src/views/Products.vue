bd8f9daabb05dac9a7d325a9dd693d8cbfa01a80
felipecabesssa <felipesoares100@hotmail.com>
2022-01-06 00:28:01 -0300

    Estilizando lista de produtos

--- frontend/src/views/Products.vue
+++ frontend/src/views/Products.vue
@@ -5,14 +5,15 @@
       <div v-for="dado in dados" :key="dado.id">
         <ul>
           <li>
-            {{ dado.id }} - {{ dado.name }} - {{ dado.amount }} - {{ dado.price }}
+            <div class="dados" id="id">{{ dado.id }}</div>
+            <div class="dados" id="name">{{ dado.name }}</div>
+            <div class="dados" id="amount">{{ dado.amount }}</div>
+            <div class="dados" id="price">{{ dado.price }}</div>
           </li>
         </ul>
       </div>
       <hr>
-      <div v-for="dado in dados" :key="dado.id" class="mt-4">
-        <div v-if="dado.category.name == 'Papelaria'">{{ dado.name }}</div>
-      </div>
+      
     </div>
   </div>
 </template>
@@ -28,6 +29,39 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .principal{
+    padding: 1%;
+    padding-top: 0.1%;
+    ul{
+      list-style: none;
+      li{
+        display: flex;
+        justify-content: space-around;
+        border: solid 1px rgb(39, 39, 39);
+        padding: 1%;
+      }
+      .dados{
+        padding: 0.7%;
+        border-right: solid 1px rgb(39, 39, 39);
+        border-left: solid 1px rgb(39, 39, 39);
+      }
+      #id{
+        min-width: 10%;
+      }
 
+      #name{
+        min-width: 35%;
+        color: rgb(209, 0, 0);
+      }
+
+      #amount{
+        min-width: 10%;
+      }
+
+      #price{
+        min-width: 14%;
+      }
+    }
+  }
 </style>
