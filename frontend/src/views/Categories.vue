95f525bf39bc83874d30bf720d287d61607b5d4a
felipecabesssa <felipesoares100@hotmail.com>
2022-01-06 02:05:16 -0300

    Tentativa de consumir dados do Backend sem sucesso - Categories

--- frontend/src/views/Categories.vue
+++ frontend/src/views/Categories.vue
@@ -1,15 +1,35 @@
 <template>
   <div>
+    <div class="categories">
       <h1>Categories</h1>
+      <!-- <div v-for="dado in dados" :key="dado.id"> -->
+      <div class="list">
+        {{ categories }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-
-}
+  data() {
+    return {
+      categories: {},
+    };
+  },
+  methods: {
+    getCategories() {
+      axios.get("http://localhost:8081/categories").then((resp) => {
+        this.categories = resp.data;
+      });
+    },
+  },
+  created() {
+    this.getCategories();
+  },
+};
 </script>
 
 <style>
-
 </style>
