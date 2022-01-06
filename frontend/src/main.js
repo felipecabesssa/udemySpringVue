6c1e89c7f08770c4309ac593d81bd14fbb12e6d9
felipecabesssa <felipesoares100@hotmail.com>
2022-01-05 22:36:54 -0300

    Limpando projeto Vue criado e Criando Menu da aplicacao

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import './scss/app.scss'
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
